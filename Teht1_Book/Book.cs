ab72a308b94b84def7ad362e877446d5bd502e85
Miika Pihkola <Miika.Pihkola@student.lab.fi>
2024-01-10 11:32:18 +0200

    First Commit

---
+++ Teht1_Book/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teht1_Book;
+
+namespace Teht1_Book
+{
+    internal class Book
+    {
+        // Field
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        // Constructor
+        public Book()
+        {
+            this.title = string.Empty;
+            this.author = string.Empty;
+            SetId(id);
+            this.price = 0;
+        }
+
+        // Method
+        private void SetId(object id)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
--- Teht1_Book/Program.cs
+++ Teht1_Book/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create Book1
+            Book book1 = new Book();
+            book1.title = "Book1";
+            book1.author = "Auth1";
+            book1.price = 24.99;
+
+            // Create Book2
+            Book book2 = new Book();
+            book2.title = "Book1";
+            book2.author = "Auth1";
+            book2.price = 24.99;
+
+
         }
     }
 }
