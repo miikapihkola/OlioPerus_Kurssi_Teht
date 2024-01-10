deffdab226ed5e342f84ce282d9dadf51d7650ac
Miika Pihkola <Miika.Pihkola@student.lab.fi>
2024-01-10 11:22:12 +0200

    Add project files.

---
+++ Teht1_Book/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Teht1_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
