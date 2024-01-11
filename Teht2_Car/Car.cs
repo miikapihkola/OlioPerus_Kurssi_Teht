3bf7fef3a44af8548c06c031fa229bc308bef685
Miika Pihkola <miika.pihkola@gmail.com>
2024-01-11 16:18:29 +0200

    Started "Teht2" Initialized "Teht4" & "Teht5"

---
+++ Teht2_Car/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teht2_Car;
+
+namespace Teht2_Car
+{
+    internal class Car
+    {
+        // Field
+        public string brand;
+        public int speed;
+
+        // Constructor
+        public Car()
+        {
+            this.brand = string.empty;
+            this.speed = 0;
+        }
+
+        // Methods
+        public void AskData()
+        {
+            
+        }
+        public void ShowCarInfo()
+        {
+
+        }
+        public void Accelerate()
+        {
+
+        }
+        public void Brake()
+        {
+
+        }
+    }
+}
---
+++ Teht4_Book2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Teht4_Book2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            winSize();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+#pragma warning disable CA1416 // Validate platform compatibility 
+        private static void winSize()
+        {
+            try { Console.SetWindowSize(75, 50); }
+            catch { Console.WriteLine("\nCould not automatically resize console window.\n\nPress any key to continue..."); Console.ReadLine(); Console.Clear(); }
+        }
+#pragma warning restore CA1416 // Validate platform compatibility
+    }
+}
