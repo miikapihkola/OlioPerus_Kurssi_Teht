ae3f2cffe080cee480b2fb66d0e47507bc3928ed
Miika Pihkola <Miika.Pihkola@student.lab.fi>
2024-01-19 13:31:21 +0200

    Started and Finished "VrtTeht1"

---
+++ VrtTeht1_Employee/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VrtTeht1_Employee
+{
+    internal class Employee
+    {
+        // Field
+        public string name;
+        private string id;
+        public string position;
+        public double salary;
+        
+
+        // Properties
+        public string Id { get => id; }
+
+        // Constructor
+        public Employee(int idCounter) 
+        {
+            this.name = string.Empty;
+            AutomaticId(idCounter);
+            this.position = string.Empty;
+            this.salary = 0;
+        }
+
+        // Methods
+        public void CompareSalary(Employee employee) 
+        {
+            if (this.salary > employee.salary) 
+                Console.WriteLine($"  {this.position,-17} {"'"+this.name+"'",-15}\tSalary: {this.salary:F2} €\n  has {this.salary-employee.salary:F2} € higher salary than\n  {employee.position,-17} '{employee.name}'\n");
+            else if (this.salary < employee.salary)
+                Console.WriteLine($"  {this.position,-17} {"'" + this.name + "'",-15}\tSalary: {this.salary:F2} €\n  has {employee.salary - this.salary:F2} € lower salary than\n  {employee.position,-17} '{employee.name}'\n");
+            else 
+                Console.WriteLine($"  {this.position,-17} '{this.name}' and\n  {employee.position,-17} '{employee.name}'\n  have equal salary of {this.salary:F2} €\n");
+        }
+        public void PrintEmployeeInfo() 
+        {
+            Console.WriteLine($"  Name:\t\t{this.name}\n  Id:\t\t{this.Id}\n  Position:\t{this.position}\n  Salary:\t{this.salary:F2} €\n");
+        }
+        private void AutomaticId(int idCounter)
+        {
+            id = string.Empty;
+            id = idCounter.ToString();
+            while (Id.Length < 5) id = "0" + id;
+        }
+        public override string? ToString()
+        {
+            return $"  Name:\t\t{this.name}\n  Id:\t\t{this.Id}\n  Position:\t{this.position}\n  Salary:\t{this.salary:F2} €\n";
+        }
+    }
+}
---
+++ VrtTeht1_Employee/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace VrtTeht1_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartFunc();
+
+            int idCounter = 0;
+            const int MaxEmployees = 10;
+
+            Employee[] employees = new Employee[MaxEmployees];
+            employees[idCounter] = new Employee(idCounter++) { name = "Aa Aa", position = "Project Manager", salary = 5000 };   // 1
+            employees[idCounter] = new Employee(idCounter++) { name = "Bb Bb", position = "Lead Artist", salary = 4000 };       // 2
+            employees[idCounter] = new Employee(idCounter++) { name = "Cc Cc", position = "Programmer", salary = 2500 };        // 3
+            employees[idCounter] = new Employee(idCounter++) { name = "Dd Dd", position = "Digital Artist", salary = 3000 };    // 4
+            employees[idCounter] = new Employee(idCounter++) { name = "Ee Ee", position = "Community Manager", salary = 3000 }; // 5
+            employees[idCounter] = new Employee(idCounter++) { name = "Ff Ff", position = "Digital Artist", salary = 3100 };    // 6
+            employees[idCounter] = new Employee(idCounter++) { name = "Gg Gg", position = "Programmer", salary = 2700 };        // 7
+            employees[idCounter] = new Employee(idCounter++) { name = "Hh Hh", position = "Programmer", salary = 3100 };        // 8
+            employees[idCounter] = new Employee(idCounter++) { name = "Jj Jj", position = "Community Manager", salary = 2900 }; // 9
+            employees[idCounter] = new Employee(idCounter++) { name = "Ii Ii", position = "Programmer", salary = 2900 };        // 10
+
+            Console.WriteLine("  EMPLOYEE INFO (using method)\n ");
+            foreach (Employee employee in employees)
+                employee.PrintEmployeeInfo();
+
+            Console.WriteLine("  EMPLOYEE INFO (using ToString)\n");
+            foreach (Employee employee in employees)
+                Console.WriteLine(employee.ToString());
+
+            idCounter = 0;
+            Console.WriteLine("  SALARY COMPARISON\n");
+            foreach (Employee employee in employees)
+            {                
+                if (employee == employees.Last())
+                { employee.CompareSalary(employees[0]); }
+                else
+                    employee.CompareSalary(employees[++idCounter]);
+            }
+
+            EndFunc();
+        }
+#pragma warning disable CA1416 // Validate platform compatibility 
+        public static void StartFunc()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string couldNot = "\nCould not automatically resize console window.\n";
+            string cont = "\nPress any key to continue...";
+            try
+            {
+                // To make this work in Win11 go to:
+                // Settings -> System -> For Developers -> Terminal -> Windows Console Host
+                // Asetukset -> Järjestelmä -> Kehittäjille -> Pääte -> Windows-konsolin isäntä
+                // However even if this does not work, this does not trigger exception when using Windows Terminal.
+                Console.SetWindowSize(75, 50);
+            }
+            catch (PlatformNotSupportedException) { Console.WriteLine($"{couldNot}PlatformNotSupportedException\n{cont}"); Console.ReadLine(); Console.Clear(); }
+            catch (System.ArgumentOutOfRangeException) { Console.WriteLine($"{couldNot}Screen is too small and/or console font is too large for set parameters.\nArgumentOutOfRangeException\n{cont}"); Console.ReadLine(); Console.Clear(); }
+            catch { Console.WriteLine($"{couldNot}{cont}"); Console.ReadLine(); Console.Clear(); }
+            Console.WriteLine();
+        }
+        public static void EndFunc()
+        {
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+#pragma warning restore CA1416 // Validate platform compatibility
+    }
+}
