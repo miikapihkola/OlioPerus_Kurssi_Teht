7bd2ef81e374c35693c909db60f2eecddab8993a
Miika Pihkola <Miika.Pihkola@student.lab.fi>
2024-02-07 15:02:11 +0200

    Started "VrtTeht3"

---
+++ VrtTeht3_Vakuutukset/Insurance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VrtTeht3_Vakuutukset
+{
+    public class Insurance
+    {
+        private string species;
+        private string name;
+        private bool neutered;
+        private double fee;
+        public Insurance(string animal, string name, bool isNeutered, double payment)
+        {
+            this.species = animal;
+            this.name = name;
+            this.neutered = isNeutered;
+            this.fee = payment;
+        }
+        public string GetNeuteredInfo()
+        {
+            if (neutered)
+            {
+                return "leikattu";
+            }
+            else
+            {
+                return "leikkaamaton";
+            }
+        }
+        public override string? ToString()
+        {
+            return this.species + ": " + this.name + ", " + this.GetNeuteredInfo();
+        }
+    }
+}
---
+++ VrtTeht3_Vakuutukset/InsuranceManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VrtTeht3_Vakuutukset
+{
+    public class InsuranceManager
+    {
+        // Field
+        public List<Insurance> insurances = new();
+
+        // Methods
+        public void AddInsurance(string animal, string name, bool isNeutered)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetFee(string species, bool isNeutererd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintInsurances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindInsurances(string species, bool isNeutered)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
